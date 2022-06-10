2b1b4de6e80fa3d074c6cd6dc9f19e27f1531687
HasanYousef <hasan.yousef.cs@gmail.com>
2022-06-10 09:09:47 +0300

    remake with fullpage.js

---
+++ script.js
@@ -0,0 +1,71 @@
+new fullpage('#fullpage', {
+    licenseKey: 'gplv3-license',
+    autoScrolling: true,
+    scrollingSpeed: 250,
+    navigation: true,
+    anchors: ['home', 'about-me', 'contact', 'trash'],
+    navigationTooltips: ['Home', 'About me', 'Contact', 'trash'],
+	slidesNavigation: true,
+    //showActiveTooltip: true,
+    parallax: true,
+    scrollOverflow: false,
+    onSlideLeave: function(section, origin, destination, direction, trigger){
+        let nextArrow = document.querySelector('.fp-controlArrow.fp-next');
+        let previousArrow = document.querySelector('.fp-controlArrow.fp-prev');
+        if(previousArrow){
+            if(destination.isFirst){
+                previousArrow.classList.add('display-none');
+            }
+            else {
+                previousArrow.classList.remove('display-none');
+            }
+        }
+        if(previousArrow){
+            if(destination.isLast){
+                nextArrow.classList.add('display-none');
+            }
+            else {
+                nextArrow.classList.remove('display-none');
+            }
+        }
+    },
+});
+
+
+
+/* MATERIAL BUTTON STARTS HERE */
+window.addEventListener('load', () => {
+    const buttons = document.querySelectorAll('.ripple');
+    if (!buttons) return;
+    for (const button of buttons) {
+        button.addEventListener('click', (e) => {
+            const rippleEffect = button.querySelector('.ripple-effect');
+            if (rippleEffect) rippleEffect.remove();
+            const posX = button.offsetLeft;
+            const posY = button.offsetTop;
+            let buttonWidth = button.offsetWidth;
+            let buttonHeight = button.offsetHeight;
+            const span = document.createElement('span');
+            if (button.classList.contains('dark')) {
+                span.classList.add('ripple-effect-dark');
+            } else {
+                span.classList.add('ripple-effect');
+            }
+            button.appendChild(span);
+    
+            if (buttonWidth >= buttonHeight) {
+                buttonHeight = buttonWidth;
+            } else {
+                buttonWidth = buttonHeight;
+            }
+            const x = e.pageX - posX - buttonWidth / 2;
+            const y = e.pageY - posY - buttonHeight / 2;
+            span.style.width = buttonWidth + 'px';
+            span.style.height = buttonHeight + 'px';
+            span.style.left = x + 'px';
+            span.style.top = y + 'px';
+            span.classList.add('ripple-animation');
+        });
+    }
+});
+/* MATERIAL BUTTON ENDS HERE */
